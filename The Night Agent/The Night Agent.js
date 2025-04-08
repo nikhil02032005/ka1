5af066ee924c027ba7f5a0aa413ce762ec20d490
nikhil <laptop02035@gmail.com>
2025-04-08 20:58:10 +0530

    updated website content

---
+++ The Night Agent/The Night Agent.js
@@ -0,0 +1,298 @@
+
+const clickCounts = {
+    Home: 0,
+    Hollywood:0,
+    Bollywood:0,
+    Punjab:0,
+    South :0,
+    Web :0,
+    GENREs:0,
+    More:0,
+    Download21:0,
+    screen:0,
+    Download20:0,
+    Download22:0,
+    Download4:0,
+    Download5:0,
+    Download6:0,
+    Download7:0,
+    Download8:0,
+    Download9:0,
+    Download10:0,
+    Download11:0,
+    Download12:0,
+    Download13:0,
+    Download14:0,
+    Download15:0,
+    Download16:0,
+    Download17:0,
+    Download18:0,
+    Download19:0,
+    Download23:0,
+    Download24:0,
+    Download25:0,
+    Download26:0,
+    Download27:0,
+
+
+}
+
+
+// Function to handle clicks on Movies, Song, Game, and End
+function handleClick(page) {
+    clickCounts[page]++;
+
+    // Get the page content to display
+    const pageContent = document.getElementById(`${page}-page`);
+    if (page === 'Home') {
+        if (clickCounts.Home === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts.Home === 2) {
+            window.open('../index.html', '_blank');
+            clickCounts.Home = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Hollywood') {
+        if (clickCounts.Hollywood === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts.Hollywood === 2) {
+            window.open('../Hollywood.html', '_blank');
+            clickCounts.Hollywood = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Bollywood') {
+        if (clickCounts.Bollywood === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts.Bollywood === 2) {
+            window.open('../bollywood.html', '_blank');
+            clickCounts.Bollywood = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Punjab') {
+        if (clickCounts.Punjab === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts.Punjab === 2) {
+            window.open('../Punjab.html', '_blank');
+            clickCounts.Punjab = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'South') {
+        if (clickCounts.South === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts.South === 2) {
+            window.open('../south.html', '_blank');
+            clickCounts.South = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Web') {
+        if (clickCounts.Web  === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts.Web  === 2) {
+            window.open('../web.html', '_blank');
+            clickCounts.Web = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'GENREs') {
+        if (clickCounts.GENREs === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts.GENREs === 2) {
+            window.open('../movies.html', '_blank');
+            clickCounts.GENREs = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'More') {
+        if (clickCounts.More === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts.More === 2) {
+            window.open('../movies.html', '_blank');
+            clickCounts.More = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download4') {
+        if (clickCounts.Download4 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download4 === 2) {
+            window.open('The Night Agent1.html', '_blank');
+            clickCounts. Download4 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download5') {
+        if (clickCounts.Download4 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download5 === 2) {
+            window.open('The Night Agent2.html', '_blank');
+            clickCounts. Download5 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download6') {
+        if (clickCounts.Download6 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download6 === 2) {
+            window.open('The Night Agent3.html', '_blank');
+            clickCounts. Download6 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download7') {
+        if (clickCounts.Download7 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download7 === 2) {
+            window.open('The Night Agent4.html', '_blank');
+            clickCounts. Download7 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download8') {
+        if (clickCounts.Download8 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download8 === 2) {
+            window.open('The Night Agent5.html', '_blank');
+            clickCounts. Download8 = 0;  // Reset click count for Movies
+        }
+    }
+
+    if (page === 'Download9') {
+        if (clickCounts.Download9 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download9 === 2) {
+            window.open('The Night Agent6.html', '_blank');
+            clickCounts. Download9 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download10') {
+        if (clickCounts.Download10 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download10 === 2) {
+            window.open('The Night Agent7.html', '_blank');
+            clickCounts. Download10 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download11') {
+        if (clickCounts.Download11 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download11 === 2) {
+            window.open('The Night Agent8.html', '_blank');
+            clickCounts. Download11 = 0;  // Reset click count for Movies
+        }
+    }
+
+    if (page === 'Download12') {
+        if (clickCounts.Download12 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download12 === 2) {
+            window.open('The Night Agent9.html', '_blank');
+            clickCounts. Download12 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download13') {
+        if (clickCounts.Download13 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download13 === 2) {
+            window.open('The Night Agent10.html', '_blank');
+            clickCounts. Download13 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download14') {
+        if (clickCounts.Download14 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download14 === 2) {
+            window.open('The Night Agent11.html', '_blank');
+            clickCounts. Download14 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download15') {
+        if (clickCounts.Download15 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download15 === 2) {
+            window.open('The Night Agent12.html', '_blank');
+            clickCounts. Download15 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download16') {
+        if (clickCounts.Download16 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download16 === 2) {
+            window.open('The Night Agent13.html', '_blank');
+            clickCounts. Download16 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download17') {
+        if (clickCounts.Download17 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download17 === 2) {
+            window.open('The Night Agent14.html', '_blank');
+            clickCounts. Download17 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download18') {
+        if (clickCounts.Download18 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download18 === 2) {
+            window.open('The Night Agent15.html', '_blank');
+            clickCounts. Download18 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download19') {
+        if (clickCounts.Download19 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download19 === 2) {
+            window.open('The Night Agent16.html', '_blank');
+            clickCounts. Download19 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download20') {
+        if (clickCounts.Download20 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download20 === 2) {
+            window.open('The Night Agent17.html', '_blank');
+            clickCounts. Download20 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download21') {
+        if (clickCounts.Download21 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download21 === 2) {
+            window.open('The Night Agent18.html', '_blank');
+            clickCounts. Download21 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download22') {
+        if (clickCounts.Download22 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download22 === 2) {
+            window.open('The Night Agent19.html', '_blank');
+            clickCounts. Download22 = 0;  // Reset click count for Movies
+        }
+    }
+    if (page === 'Download23') {
+        if (clickCounts.Download23 === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. Download23 === 2) {
+            window.open('The Night Agent20.html', '_blank');
+            clickCounts. Download23 = 0;  // Reset click count for Movies
+        }
+    }
+    
+    if (page === 'screen') {
+        if (clickCounts.screen === 1) {
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
+        } else if (clickCounts. screen === 2) {
+            window.open('movies.html', '_blank');
+            clickCounts. screen = 0;  // Reset click count for Movies
+        }
+    }
+}
+        // When the thumbnail is clicked, show the video player
+        document.getElementById("thumbnail").addEventListener("click", function() {
+            // Hide the thumbnail image
+            document.getElementById("thumbnail").style.display = "none";
+            
+            // Show the video player
+            document.getElementById("videoPlayerContainer").style.display = "block";
+            
+            // Play the video
+            const video = document.getElementById("videoPlayer");
+            video.play();
+        });
+        function toggleMenu() {
+            const nav = document.getElementById('content');
+            nav.classList.toggle('show');
+          }
